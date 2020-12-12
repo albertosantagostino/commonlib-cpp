f82a0b30f6ede737d64628b623addf195c077be4
Alberto Santagostino <santagostino.alb@gmail.com>
2020-12-12 18:47:18 +0100

    Improve Position

--- include/primitives/position.h
+++ include/primitives/position.h
@@ -15,13 +15,23 @@ class Position
   public:
     Position();
     Position(T x, T y);
+    Position(std::pair<T, T> xy_pair);
     Position<T> operator+=(const Position<T>& other);
+    Position<T> operator*(const int other);
     T x;
     T y;
 };
 
 template <typename T>
+Position<T>::Position() : x(), y()
+{}
+
+template <typename T>
 Position<T>::Position(T x, T y) : x(x), y(y)
+{}
+
+template <typename T>
+Position<T>::Position(std::pair<T, T> xy_pair) : x(xy_pair.first), y(xy_pair.second)
 {}
 
 template <typename T>
@@ -33,13 +43,20 @@ Position<T> Position<T>::operator+=(const Position<T>& other)
 }
 
 template <typename T>
+Position<T> Position<T>::operator*(const int other)
+{
+    Position ret;
+    ret.x = x * other;
+    ret.y = y * other;
+    return ret;
+}
+
+template <typename T>
 Position<T> operator+(const Position<T>& p1, const Position<T>& p2)
 {
-    T x{p1.x + p2.x};
-    T y{p1.y + p2.y};
     Position<T> ret;
-    ret.x = x;
-    ret.y = y;
+    ret.x = p1.x + p2.x;
+    ret.y = p1.y + p2.y;
     return ret;
 }
 
